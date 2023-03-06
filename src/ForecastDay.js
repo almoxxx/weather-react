5473f2c9f68e9cc3e6adb3d11f367bdb9052029f
almoxxx <120190420+almoxxx@users.noreply.github.com>
2023-03-06 09:38:10 +0100

    Update Forecast

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function ForecastDay(props) {
-  function day(props) {
+  function day() {
     let date = new Date(props.data.time * 1000);
     let day = date.getDay();
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -12,7 +12,7 @@ export default function ForecastDay(props) {
     <div className="forecastDay">
       <div className="forecast-day">{day()}</div>
       <img
-        src={props.data.condition.description}
+        src={props.data.condition.icon_url}
         alt={props.daily.condition.description}
       />
       <div className="forecast-temperatures">
